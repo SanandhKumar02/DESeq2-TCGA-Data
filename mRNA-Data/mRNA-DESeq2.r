d4e92fb0e8b4841411eea8707f54bb116b0da565
Sanandh Kumar M <sanandhkumarm@Sanandhs-MacBook-Air.local>
2022-07-07 21:58:47 +0530

    Downloading Data and Processing

--- mRNA-Data/mRNA-DESeq2.r
+++ mRNA-Data/mRNA-DESeq2.r
@@ -25,3 +25,41 @@ get_downregulated <- function(df){
   results <- as.data.frame((df)[which(rownames(df) %in% key),])
   return(results)
 }
+
+#Downloading and Data Wrangling
+
+query <- GDCquery(project = "TCGA-PRAD", 
+                  data.category = "Gene expression",
+                  data.type = "Gene expression quantification",
+                  experimental.strategy = "RNA-Seq",
+                  platform = "Illumina HiSeq",
+                  file.type = "results",
+                  legacy = TRUE)
+GDCdownload(query, method = "api", files.per.chunk = 100,
+            directory = "/content/Data")
+mrna_df <- GDCprepare(query, directory = "/content/Data")
+
+# Remove columns we dont need, keep counts
+
+mrna_meta <- mrna_df$sample
+mrna_meta <- cbind(mrna_meta, mrna_df$definition)
+mrna_df <- assay(mrna_df)
+delim_fn = function(x, n, i){
+    do.call(c, lapply(x, function(X)
+        paste(unlist(strsplit(X, "-"))[(n+1):(i)], collapse = "-")))
+}
+colnames(mrna_df) <- delim_fn(x = colnames(mrna_df), n = 0, i = 4)
+mrna_meta <- as.data.frame(mrna_meta)
+mrna_df <- as.data.frame(mrna_df)
+
+# Remove metastatic sample
+
+metastatic_key <- mrna_meta[which(mrna_meta[,2] == "Metastatic"),]
+mrna_meta <- mrna_meta[!mrna_meta[,2] == metastatic_key[,2],]
+mrna_df <- mrna_df[, -grep(paste0(metastatic_key[,1]), colnames(mrna_df))]
+mrna_meta[,2] <- as.character(mrna_meta[,2])
+mrna_meta[,2] <- gsub("Primary solid Tumor", "Tumor", mrna_meta[,2])
+mrna_meta[,2] <- gsub("Solid Tissue Normal", "Normal", mrna_meta[,2])
+mrna_meta[,2] <- as.factor(mrna_meta[,2])
+levels(mrna_meta[,2])
+colnames(mrna_meta) <- c("cases", "Condition")
